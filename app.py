e091269f85c5595e06248903f0f4674ebf65dea3
deanrivers <deanriversdeveloper@gmail.com>
2020-03-07 22:08:01 -0500

    Updated. Works for 1st iteration of while loop

--- app.py
+++ app.py
@@ -5,96 +5,118 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 import tkinter as tk
 from tkinter import *
 
 root = tk.Tk()
 
-#scroll function
-def scroll(driver,scroll_height):
-    driver.execute_script("window.scrollTo(0,"+str(scroll_height)+")") 
-
 #like function
-def like(driver):
-    clicked_elements = []
-    like_buttons_container = driver.find_elements_by_class_name('fr66n')
-    # print('Like button set:',like_buttons_container)
-    print(like_buttons_container.session())
-
-    # for items in like_buttons_container
-    #     like_button = items.find_element_by_tag_name('button')
-    #     if like_button not in clicked_elements:
-    #         like_button.click()
-        
-    #like_buttons[0].find_element_by_tag_name('button').click()
-
+def like(driver,clicked_elements):
+    clicked_arr = clicked_elements
+    queue_arr = []
     
 
+    try:
+        like_buttons_container = driver.find_elements_by_class_name('fr66n')
+    except Exception as e:
+        print('Error selecting class.',e)
 
-#general process
-def login(config):
-    #variables from gui
+    #put into global clicked_arr and queue_arr for this iteration
+    for items in like_buttons_container:
+        try:
+            like_button = items.find_element_by_tag_name('button')
+            if like_button not in clicked_arr:
+                clicked_arr.append(like_button)
+                queue_arr.append(like_button)
+        except Exception as e:
+            print('Outter Loop error',e)
+
+    #iterate and click
+    print('Queue before loop:',queue_arr)
+    for button in queue_arr:
+        print('b4')
+        actions = ActionChains(driver)
+        actions.move_to_element(button).perform()
+        print('af')
+        print('scroll performed for:',button)
+        time.sleep(2)
+        
+        try:
+            button.click()
+            print('---')
+            print('---')
+            print('---')
+            print('An element was clicked:',button)
+            print('---')
+            print('---')
+            print('---')
+        except Exception as e:
+            print('Error clicking for',button)
+
+    #like_buttons[0].find_element_by_tag_name('button').click()
+    print('Clicked array for this iteration:',clicked_arr)
+    final_element = clicked_arr[len(clicked_arr)-1]
+    return clicked_arr
+
+def login(driver):
+     #variables from gui
     username_text = username_input.get()
     password_text = password_input.get()
-    print('username',username_text)
-    print('password',password_text)
 
-    #driver = webdriver.Chrome()
-    chrome_options = Options()
-    driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver',options=chrome_options)
-    
-    #instagram 
-    driver.get("https://instagram.com")
-    
     try:
-        # click login button
-        time.sleep(2)
-        driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[2]/p/a').click()
-
         #enter info
         time.sleep(2)
+        
+        #grab elements
         user_name = driver.find_element_by_name('username')
         password = driver.find_element_by_name('password')
         
+        #send GUI info to elements
         user_name.send_keys(username_text)
         password.send_keys(password_text)
 
         #click login
         time.sleep(2)
-        driver.find_element_by_xpath('/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div[4]/button').click()
+        #driver.find_element_by_xpath('/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div[4]/button').click()
+        driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]').click()
         
         #remove notification if it pops up
-        time.sleep(2)
-        driver.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[2]').click()
-
-        #enter while loop for a specified number of iterations
-        i = 0
-        scroll_height = 0
-        while i < 3:
-            print('Iteration number:',i)
-            print('Scroll height:',scroll_height)
-
-            #scroll down to load more content
-            time.sleep(2)
-            scroll(driver,scroll_height)
-
-            #gather all like buttons and iterate
-            time.sleep(2)
-            like(driver)
-            
-            #iterate and icrease scroll height
-            i = i+1
-            scroll_height = 1080*i
-        
-        #out of loop
-        print('loop ended')
-
+        try:
+            time.sleep(3)
+            driver.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[2]').click()
+        except Exception as e:
+            print('Click notification error:',e)
     except Exception as e:
-        print(e)
+        print('Login error:',e)
 
-    finally:
-        driver.quit()
+#general process
+def launch(config):
+    chrome_options = Options()
+    driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver',options=chrome_options)
+    driver.get("https://instagram.com")
+    
+    #login
+    login(driver)
+
+    #clicked elements
+    done_elements = []
+
+    #enter while loop for a specified number of iterations
+    i = 0
+    scroll_height = 0
+    while i < 4:
+        print('Iteration number:',i)
+        x = like(driver,done_elements)
+        done_elements = x
+        i = i+1
+
+    print('while loop ended')
+    print('Clicked Elements:',done_elements)
+    print('EVERYTHING WAS CLICKED CORRECTLY!!!')
+
+
 
 #tkinter
 canvas = tk.Canvas(root,height=500,width=500,bg="#ddd")
@@ -119,11 +141,11 @@ password_label = Label(root, text="password", font=("Helvetica",14),bg=backgroun
 canvas.create_window(250,290,window=password_label)
 
 #password input
-password_input = tk.Entry(root)
+password_input = tk.Entry(root,show="*")
 canvas.create_window(250,320,window=password_input)
 
 #submit button
-button = tk.Button(text='Submit', command=lambda:login(config))
+button = tk.Button(text='Submit', command=lambda:launch(config))
 canvas.create_window(250, 420, window=button)
 
 #build GUI
