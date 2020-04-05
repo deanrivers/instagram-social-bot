729fec3293c4998c4c15e6ab88dd8ebad6c86dca
deanrivers <deanriversdeveloper@gmail.com>
2020-04-04 22:14:10 -0400

    Updated last button reference and scroll method.

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def login(driver):
      #variables from gui
     username_text = username_input.get()
     password_text = password_input.get()
+    limit_text = limit_input.get()
 
     try:
         #enter info
@@ -31,12 +32,11 @@ def login(driver):
 
         #click login
         time.sleep(2)
-        #driver.find_element_by_xpath('/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div[4]/button').click()
         driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]').click()
         
         #remove notification if it pops up
         try:
-            time.sleep(3)
+            time.sleep(2)
             driver.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[2]').click()
         except Exception as e:
             print('Click notification error:',e)
@@ -44,101 +44,113 @@ def login(driver):
         print('Login error:',e)
 
 #like function
-def like(driver):
+def like(driver,like_limit):
     clicked_arr = []
     queue_arr = []
+    num_likes = 0
     
-    
-    
-    while True:
+    while num_likes < like_limit:
         try:
             #put button into list
             like_buttons = driver.find_elements_by_css_selector('span.fr66n>button')
-            # like_buttons = driver.find_elements_by_css_selector('div._97aPb ')
             print('Like buttons selected:',like_buttons)
         except Exception as e:
-            print('Error selecting button.',e)
+            print('Error selecting buttons.',e)
 
         #loop through buttons
         #try and click if it does not exist in clicked_arr
         for button in like_buttons:
-            if button not in clicked_arr:
-                try:
-                    actions = ActionChains(driver)
-                    actions.move_to_element(button).perform()
+            try:
+                actions = ActionChains(driver)
+                actions.move_to_element(button).perform()
 
-                    #append to clicked queue
-                    clicked_arr.append(button)
-                    time.sleep(2)
+                #append to clicked queue
+                clicked_arr.append(button)
+                time.sleep(1)
 
-                    
-                    button.click()
-                    print('button clicked')
+                button.click()
+                num_likes = num_likes + 1
+                print('Like number:',num_likes)
 
-                    # actions.double_click(button)
-                    print('---')
-                    print('---')
-                    print('---')
-                    print('An element was clicked:',button)
-                    print('---')
-                    print('---')
-                    print('---')
-                    time.sleep(2)
-                except Exception as e:
-                    print('Error clicking for',button,e)
+                print('---')
+                print('---')
+                print('---')
+                print('An element was clicked:',button)
+                print('---')
+                print('---')
+                print('---')
+                time.sleep(1)
+            except Exception as e:
+                print('Error clicking for',button,e)
 
         # re-analyze DOM. Jump to last item in list
-        print('reanalyze')
+        print('-')
+        print('--')
+        print('---')
+        print('----')
+        print('-----')
+        print('------')
+        print('-------')
+        print('Reanalyzing...once last element is no longer in DOM, new elements will be ready to be clicked.')
+        print('-------')
+        print('------')
+        print('-----')
+        print('----')
+        print('---')
+        print('--')
+        print('-') 
+
         try:
-            re_buttons = driver.find_elements_by_css_selector('span.fr66n>button')
-            last_button = re_buttons[-1]
-            
+            #scroll to top to get the correct 'last_button' needed
+            driver.execute_script("window.scrollTo(0,0)")
+            reanalyzed_buttons = driver.find_elements_by_css_selector('span.fr66n>button')
+            last_button = reanalyzed_buttons[-1]
+            last_button_scroll_height = last_button.location['y']
+            print('last button:',last_button)
+            print(last_button_scroll_height)
+
         except Exception as e:
             print('Re-analayze issue:',e)
 
         #place listener on last item
-
-
         #jump to last item
         try:
-            actions = ActionChains(driver)
-            actions.move_to_element(last_button).perform()
+            driver.execute_script("window.scrollTo(0,"+str(last_button_scroll_height)+")")
+            # actions = ActionChains(driver)
+            # actions.move_to_element(last_button).perform()
             print('jump performed')
         except Exception as e:
             print('jump error:',e)
         
         #programatically scroll and stop once listener detects that the last button has been removed from the DOM
         scroll_continue = True
-        scroll_height = last_button.location['y']
+        
 
         while scroll_continue:
             print('scroll')
             time.sleep(1)
 
-            #determine if element still exists
-            #try and scoll to it
+            #determine if element still exists by trying to scroll to it
             try:
                 actions = ActionChains(driver)
                 actions.move_to_element(last_button).perform()
                 print('rescroll performed')
                 
             except Exception as e:
-                print('Last button in list no longer exists:',e)
+                print('Last button in list no longer exists...new elements are available')
                 scroll_continue = False
             else:
-                print('Total scroll:',scroll_height)
-                driver.execute_script("window.scrollTo(0,"+str(scroll_height)+")")
-                scroll_height = scroll_height+500
-                
+                print('Total scroll:',last_button_scroll_height)
+                driver.execute_script("window.scrollTo(0,"+str(last_button_scroll_height)+")")
+                last_button_scroll_height = last_button_scroll_height+500
 
     #scroll to top
     driver.execute_script("window.scrollTo(0,0)")
-    # actions.move_to_element(clicked_arr[0]).perform()
-    print('scroll to top.')
-    print('clicked arr:',clicked_arr)
+    
+    print('scrolled to top.')
+    print('All elements that were clicked:',clicked_arr)
 
     time.sleep(5)
-    new_arr = []
 
     #like_buttons[0].find_element_by_tag_name('button').click()
     return clicked_arr
@@ -158,17 +170,15 @@ def launch(config):
     login(driver)
 
     #enter while loop for a specified number of iterations
-    like(driver)
-    
+    like_limit = int(limit_input.get())
+    like(driver,like_limit)
 
-    # print('while loop ended')
-    # print('Clicked Elements:',done_elements)
     print('EVERYTHING WAS CLICKED CORRECTLY!!!')
 
 
 
 #tkinter
-canvas = tk.Canvas(root,height=500,width=500,bg="#ddd")
+canvas = tk.Canvas(root,height=600,width=500,bg="#ddd")
 canvas.pack()
 
 background = '#ddd'
@@ -193,9 +203,20 @@ canvas.create_window(250,290,window=password_label)
 password_input = tk.Entry(root,show="*")
 canvas.create_window(250,320,window=password_input)
 
+#limit label
+limit_label = Label(root, text="like limit", font=("Helvetica",14),bg=background)
+canvas.create_window(250,410,window=limit_label)
+
+#limit input
+limit_input = tk.Entry(root)
+canvas.create_window(250,440,window=limit_input)
+
+#limit dropdown
+
+
 #submit button
 button = tk.Button(text='Submit', command=lambda:launch(config))
-canvas.create_window(250, 420, window=button)
+canvas.create_window(250, 500, window=button)
 
 #build GUI
 root.resizable(False, False)
